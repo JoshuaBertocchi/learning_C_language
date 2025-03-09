8c1c1a9360ec69d1c3983d08ca28fe81fde1c51c
JoshuaBertocchi <joshuabertocchi@gmail.com>
2025-03-09 17:46:57 -0300

    feat new código

--- linguagem_C/Curso_Linguagem_C_youtube/aula_if_else.c
+++ linguagem_C/Curso_Linguagem_C_youtube/aula_if_else.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
+#include <locale.h> //Biblioteca para o c√≥digo executar acentos
+#include <stdlib.h>
 int main(){
+    setlocale(LC_ALL, "Portuguese_Brazil"); // Interpreta caracteres da lingua portuguesa
 
     int A, B, soma, subtr, mult, divis, res_divisao, resultado;
 
@@ -16,16 +18,28 @@ int main(){
     divis = A / B;
     res_divisao = A % B;
     
-    printf("Escolha a op√ß√£o abaixo:\n[1]Soma\n[2]Subtra√ß√£o\n[3]Multiplica√ß√£o\n[4]Divis√£o\n");
-    scanf("%d", resultado);
+    printf("Escolha a opÁ„o abaixo:\n[1]Soma\n[2]SubtraÁ„o\n[3]MultiplicaÁ„o\n[4]Divis„o\n");
+    scanf("%d", &resultado);
 
     if (resultado == 1){
         printf("Soma: %d.\n", soma); 
     }
-
-    
-    printf("Subtra√ß√£o:%d.\n", subtr);
-    printf("Multiplica√ß√£o:%d.\n",mult);
-    printf("Divis√£o inteira:%d. \n", divis);
-    printf("Resto da divis√£o: %d.\n", res_divisao);
+    else if (resultado == 2){
+        printf("SubtraÁ„o: %d\n", subtr);
+    }
+    else if (resultado == 3){
+        printf("MultiplicaÁ„o: %d\n", divis);
+    }
+    else if (resultado == 4){
+        printf("Divis„o inteira: %d\n", divis, res_divisao);
+        if (res_divisao > 0){
+            printf("Resto da divis„o: %d\n", res_divisao);
+        }
+        else {
+            printf("N„o possui resto da divis„o!");
+        }
+    }
+    printf("teste\n");
+    system("pause");
+    return 0;
 }
